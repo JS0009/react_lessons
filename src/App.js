c22954d05e92d8adca81c91f725d0fe4ab6870c1
maga_bash <caitov.magomed@yandex.ru>
2022-04-24 21:52:03 +0300

    Coponetn-Counter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React, { useState } from 'react';
 import './App.css';
+import Counter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = useState(7)
-  const [value, setValue] = useState('GitHab')
-  function increment() {
-    setLikes(likes + 1)
-  }
-
-  function decrement() {
-    setLikes(likes - 1)
-  }
   return (
-    <div className='App'>
-      <h1>{likes}</h1> 
-      <h1>{value}</h1>
-      <button onClick={increment}>increment</button>
-      <button onClick={decrement}>decrement</button>
-      <input type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)} />
-   </div>
+   <Counter/>
   );
 }
 
