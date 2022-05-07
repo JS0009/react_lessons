997a991be75b04c509daba284bb5024a8f92658a
maga_bash <caitov.magomed@yandex.ru>
2022-05-07 15:57:45 +0300

    onChange

--- src/App.js
+++ src/App.js
@@ -6,25 +6,33 @@ import MyInput from './components/UI/input/MyInput';
 
 
 function App() {
-  
+
   const [post, setPost] = useState([
-    {id: 1, title: 'JavaScript',body: 'JavaScript - язык программирования'},
-    {id: 2, title: 'JavaScript',body: 'JavaScript - язык программирования'},
-    {id: 3, title: 'JavaScript',body: 'JavaScript - язык программирования'},
-    {id: 4, title: 'JavaScript',body: 'JavaScript - язык программирования'}
+    { id: 1, title: 'JavaScript', body: 'JavaScript - язык программирования' },
+    { id: 2, title: 'JavaScript', body: 'JavaScript - язык программирования' },
+    { id: 3, title: 'JavaScript', body: 'JavaScript - язык программирования' },
+    { id: 4, title: 'JavaScript', body: 'JavaScript - язык программирования' }
   ])
 
- 
+  const [title, setTitle] = useState('0')
+  const addNewPost = () => {
+
+  }
 
   return (
-    
+
     <div className='App'>
       <form>
-        <MyInput type="text" placeholder='название поста'/>
-        <MyInput type="text" placeholder='описание поста'/>
-        <MyButton disabled>Создать пост</MyButton>
+        {/* Управляемый компонет */}
+        <MyInput
+          value={title}
+          onChange={e => setTitle(e.target.value)}
+          type="text"
+          placeholder='название поста' />
+        <MyInput type="text" placeholder='описание поста' />
+        <MyButton disabled onClick={addNewPost}>Создать пост</MyButton>
       </form>
-      <PostList posts={post} title="Посты про js"/>
+      <PostList posts={post} title="Посты про js" />
     </div>
 
   );
